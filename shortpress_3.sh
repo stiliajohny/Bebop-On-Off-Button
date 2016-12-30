2ac2e74ddf47a5765b57b0a1c117c276f47b101d
stiliajohny <stilia.johny@gmail.com>
2016-12-30 19:35:23 +0000

    Edit scripts 3, 5, 6, 7
    Ammend scripts to have a sound feedback after the script has finish.

--- shortpress_3.sh
+++ shortpress_3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Script called when pressing the ON/OFF twice once
+# Script called when pressing the ON/OFF twice
 
 BLDC_Test_Bench -M 2 &
--- shortpress_5.sh
+++ shortpress_5.sh
@@ -4,19 +4,25 @@
 INT_MEM_DIRECTORY=/data/ftp/internal_000/ BOP_MEDIA_DIRECTORY=/data/ftp/internal_000/Bebop_Drone/media/ USB_OTG_DIRECTORY=
 # search for USB OTG directory using pattern *_nnn
 for directory in $(find /data/ftp/ -maxdepth 1 -type d -regex ".*_[0-9][0-9]*"); do
-if [ "$directory/" != "$INT_MEM_DIRECTORY"Bebop_Drone/media/ ]; then USB_OTG_DIRECTORY="$directory"/Bebop_Drone/media/
-break
-fi
+  if [ "$directory/" != "$INT_MEM_DIRECTORY"Bebop_Drone/media/ ]; then
+   USB_OTG_DIRECTORY="$directory"/Bebop_Drone/media/
+   break
+ fi
 done
-# copy the files if we have USB OTG drive mounted if [ -d "$USB_OTG_DIRECTORY" ]; then
-echo "Moving media to USB OTG drive ($USB_OTG_DIRECTORY)..."
-# Orange LED during copy (BLDC_Test_Bench -G 1 1 0 >/dev/null) &
-cp -f "$BOP_MEDIA_DIRECTORY"* "$USB_OTG_DIRECTORY" # Green LED = copy finished
-(BLDC_Test_Bench -G 0 1 0 >/dev/null) &
-#creates copy_ok file when the copy process is finished touch "$INT_MEM_DIRECTORY"Bebop_Drone/copy_ok
+# copy the files if we have USB OTG drive mounted
+
+if [ -d "$USB_OTG_DIRECTORY" ]; then
+  echo "Moving media to USB OTG drive ($USB_OTG_DIRECTORY)..."
+  # Orange LED during copy
+  (BLDC_Test_Bench -G 1 1 0 >/dev/null) &
+  cp -f "$BOP_MEDIA_DIRECTORY"* "$USB_OTG_DIRECTORY" # Green LED = copy finished
+  (BLDC_Test_Bench -G 0 1 0 >/dev/null) & # Startup sound = copy finished
+  (BLDC_Test_Bench -M 1 )&
+  #creates copy_ok file when the copy process is finished
+  touch "$INT_MEM_DIRECTORY"Bebop_Drone/copy_ok
 else
-echo "USB OTG drive not mounted!"
-# Red LED flashes for 3 seconds to notify the error
-(BLDC_Test_Bench -G 1 0 1 >/dev/null; sleep 3; BLDC_Test_Bench -G 0 1 0 >/dev/null) &
+  echo "USB OTG drive not mounted!"
+  # Red LED flashes for 3 seconds to notify the error
+  (BLDC_Test_Bench -G 1 0 1 >/dev/null; sleep 3; BLDC_Test_Bench -G 0 1 0 >/dev/null) &
 fi
 BLDC_Test_Bench -M 2 &
